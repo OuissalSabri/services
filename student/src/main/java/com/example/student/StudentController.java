90e0e13bd9bb7ce185254c5e2a66eb73c19e0ae8
OuissalSabri <sabri.ouissal1@gmail.com>
2024-01-19 02:23:48 +0100

    first commit

---
+++ student/src/main/java/com/example/student/StudentController.java
@@ -0,0 +1,35 @@
+package com.example.student;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/students")
+@RequiredArgsConstructor
+public class StudentController {
+
+    private final StudentService service;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void save(
+            @RequestBody Student student
+    ){
+        service.saveStudent(student);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Student>> findAllStudent(){
+      return ResponseEntity.ok(service.findAllStudent());
+    }
+
+    @GetMapping("/school/{school-id}")
+    public ResponseEntity<List<Student>> findAllStudent(@PathVariable("school-id") Integer schoolId){
+
+        return ResponseEntity.ok(service.findAllStudentsBySchool(schoolId));
+    }
+}
